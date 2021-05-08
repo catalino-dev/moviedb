0e38bbd4560d32d719ed4208fd0988411c2e01bc
JR Comision <saviercoo@gmail.com>
2021-05-08 20:31:05 +0800

    Fix request mapping and exception handling

---
+++ src/main/java/com/ccomision/moviedb/exception/ApiError.java
@@ -0,0 +1,19 @@
+package com.ccomision.moviedb.exception;
+
+import lombok.Getter;
+
+@Getter
+enum ApiError {
+    MOVIE_NOT_FOUND("The movie could not be found.");
+
+    private final String errorCode = this.name();
+    private final String errorMessage;
+
+    ApiError(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String toString() {
+        return this.errorCode;
+    }
+}
